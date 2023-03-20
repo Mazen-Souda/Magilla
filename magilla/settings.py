b64954cdea838ae5bd79415e977b5640c5626750
EMPEROR <mazensouda@gmail.com>
2023-03-20 14:10:55 +0300

    Configure Django Media Files

--- magilla/settings.py
+++ magilla/settings.py
@@ -125,3 +125,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# media files configuration
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR /'media'
