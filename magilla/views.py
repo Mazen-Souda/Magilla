9b7da0cc7af51e8c17ca962de393135fabc8309e
EMPEROR <mazensouda@gmail.com>
2023-03-24 16:54:55 +0300

    Display Product in Homepage

--- magilla/views.py
+++ magilla/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    prodects=Product.objects.all().filter(is_available=True)
+    context={
+        'products':prodects,
+    }
+    return render(request, 'home.html',context)
